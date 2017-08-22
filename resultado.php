8da1aac5a07d54b404b7d91f935a996a1416166e
Caiorodrigues99 <caiorodrigues@visaojr.com.br>
2017-08-21 22:49:25 -0300

    vamo q vamo kkk

--- resultado.php
+++ resultado.php
@@ -78,48 +78,129 @@ if(isset($_POST['insert'])){
 	<body>
 		<div class="row">
 			<div class="container" align="center">
-				<h1>Trabalho de Economia</h1>
+				<div align="center">
+				<a href="#" ><img src="imagens/DIADEROCK.png" height="200px" width="200px" align="center"></a></div>
 				
 				<div class="col-md-4">
+				<?php 
+				if(isset($_POST["agua"]) == 1){
+					echo <<<HTML
 				<h2>Quantidade de Agua</h2>
-				<h3><?php echo $agua ?>Ml</h3>
+				<a href="#"><img src="imagens/agua.png" height="150px" width="150px"></a>
+				<h3> $agua Ml.</h3>
+HTML;
+				}
+				 ?>
+				<?php 
+				if(isset($_POST["cerveja"]) == 1){
+					echo <<<HTML
 				<h2>Quantidade de Cerveja</h2>
-				<a href="#"><img src="imagens/b1.png"></a>
-				<h3><?php echo $cerveja ?>Ml</h3>
+				<a href="#"><img src="imagens/b1.png" height="150px" width="180px"></a>
+				<h3> $cerveja Ml.</h3>
+HTML;
+				}
+				 ?>
+				 <?php 
+				if(isset($_POST["refri"]) == 1){
+					echo <<<HTML
 				<h2>Quantidade de Refrigerante</h2>
-				<h3><?php echo $refri ?>Ml</h3>
+				<a href="#"><img src="imagens/refri.png" height="150px" width="150px"></a>
+				<h3> $refri Ml.</h3>
+HTML;
+				}
+				 ?>
+				 <?php 
+				if(isset($_POST["suco"]) == 1){
+					echo <<<HTML
 				<h2>Quantidade de Suco</h2>
-				<h3><?php echo $suco ?>Ml</h3>
-				</div>
-
-				<div class="col-md-4">
-				<h2>Quantidade de Carne de Boi</h2>
-				<a href="#"><img src="imagens/c1.png"></a>
-				<h3><?php echo $boi ?>g</h3>
-				<h2>Quantidade de Carne de Porco</h2>
-				<h3><?php echo $porco ?>g</h3>
-				<h2>Quantidade de Linguiça</h2>
-				<h3><?php echo $ling ?>g</h3>
-				<h2>Quantidade de Carne de Frango</h2>
-				<h3><?php echo $frango ?>g</h3>
-				</div>
-
-
-				<div class="col-md-4">
+				<a href="#"><img src="imagens/suco.png" height="150px" width="150px"></a>
+				<h3> $suco Ml.</h3>
+HTML;
+				}
+				 ?>
 				
-				<h2>Quantidade de Pratos</h2>
-				<h3><?php echo $prato ?>Uni.</h3>
-				<h2>Quantidade de Copos</h2>
-				<h3><?php echo $copo ?>Uni.</h3>
-				<h2>Quantidade de Talheres</h2>
-				<h3><?php echo $talher ?>Conjuntos</h3>
+				
 				</div>
 
-				<a href="index.php"><button class="btn btn-primary">Voltar</button></a>
+				<div class="col-md-4">
+						<?php 
+				if(isset($_POST["boi"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Carne de Boi </h2>
+				<a href="#"><img src="imagens/c1.png" height="150px" width="150px"></a>
+				<h3> $boi g.</h3>
+HTML;
+				}
+				 ?>
+				<?php 
+				if(isset($_POST["porco"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Carne de Porco</h2>
+				<a href="#"><img src="imagens/porco.png" height="150px" width="150px"></a>
+				<h3> $porco g</h3>
+HTML;
+				}
+				 ?>
+				<?php 
+				if(isset($_POST["ling"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Linguiça</h2>
+				<a href="#"><img src="imagens/ling.jpg" height="150px" width="150px"></a>
+				<h3> $ling g</h3>
+HTML;
+				}
+				 ?>
+				<?php 
+				if(isset($_POST["frango"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Carne de Frango</h2>
+				<a href="#"><img src="imagens/frango.jpg" height="150px" width="180px"></a>
+				<h3> $frango g</h3>
+HTML;
+				}
+				 ?>
+			
+				</div>
+
+
+				<div class="col-md-4">
+				<?php 
+				if(isset($_POST["prato"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Pratos</h2>
+				<a href="#"><img src="imagens/pratos.png" height="150px" width="150px"></a>
+				<h3> $prato Uni.</h3>
+HTML;
+				}
+				 ?>
+				 		<?php 
+				if(isset($_POST["copo"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Copos</h2>
+				<a href="#"><img src="imagens/copos.png" height="150px" width="150px"></a>
+				<h3> $copo Uni.</h3>
+HTML;
+				}
+				 ?>
+				 		<?php 
+				if(isset($_POST["talher"]) == 1){
+					echo <<<HTML
+				<h2>Quantidade de Talheres</h2>
+				<a href="#"><img src="imagens/talheres.jpg" height="150px" width="150px"></a>
+				<h3> $talher Conjutos </h3>
+HTML;
+				}
+				 ?>
+				
+				
+				</div>
+
+				
 			
 				
 			</div>
 		</div>
+		<center><a href="index.php"><button class="btn btn-primary">Voltar</button></a></center>
 	</body>
 	</html>
 	<?php
