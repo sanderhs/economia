45098eadc5a387bbb6c41278d6bd32db501faee3
Caiorodrigues99 <caiorodrigues@visaojr.com.br>
2017-08-22 07:45:55 -0300

    Codigo comentado

--- index.php
+++ index.php
@@ -1,98 +1,99 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Economia</title>
+	<title>Hoje é Dia de Rock</title>
+	<!-- importaçoes -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="imagens/DIADEROCK.png" type="image/x-icon" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-	
 </head>
 <body>
 	<div class="row">
-				<div align="center">
-				<a href="#" ><img src="imagens/DIADEROCK.png" height="200px" width="200px" align="center"></a></div>
-				
+		<div align="center">
+			<a href="index.php" ><img src="imagens/DIADEROCK.png" height="200px" width="200px" align="center"></a>
+		</div>	
 		<div id="conteiner" class="container">
 			<form action="resultado.php" method="post" class="form-horizontal">
-			<div class="row" align="center">
-			
-			<div class="col-md-4"></div>
-			<div id="pessoas" class="form-group col-md-4">
-				<h3>Pessoas</h3>
-				<img src="imagens/team.png" height="80px" width="80px" align="center">
-				<label>Quantas pessoas vão no rolê?</label>
-
-				<input type="number" class="form-control" name="quantidade" placeholder="20" required="">
-			</div>
-			</div>
-			<br><br>
-			<div id="bebidas" class="col-md-4" align="center">
-			<img src="imagens/pint.png" height="80px" width="80px">
-			<h3>Bebidas</h3>
-			    <div class="form-group" align="left">
+				<div class="row" align="center">
+					<div class="col-md-4"></div>
+					<div id="pessoas" class="form-group col-md-4">
+						<h3>Pessoas</h3>
+						<img src="imagens/team.png" height="80px" width="80px" align="center">
+						<label>Quantas pessoas vão no rolê?</label>
+						<input type="number" class="form-control" name="quantidade" placeholder="20" required="">
+					</div><!-- Fim do formgroup -->
+				</div><!-- Fim da row -->
+				<br><br>
+				<!-- Div da parte de bebidas do form -->
+				<div id="bebidas" class="col-md-4" align="center">
+					<img src="imagens/pint.png" height="80px" width="80px">
+					<h3>Bebidas</h3>
+					<div class="form-group" align="left">
+						<br><br>
+						<!-- Checkbox de bebidas -->	
+						<div class="checkbox">
+							<label><input type="checkbox" name="agua" value="on">Agua</label>
+						</div>
+						<div class="checkbox">
+							<label><input type="checkbox" name="cerveja" value="1">Cerveja</label>
+						</div>
+						<div class="checkbox">
+							<label><input type="checkbox" name="refri" value="1">Refrigerante</label>
+						</div>
+						<div class="checkbox">
+							<label><input type="checkbox" name="suco" value="1">Suco</label>
+						</div>
+					</div><!-- Fim formgroup -->
+				</div><!-- Fim da div bebidas -->
+				<!-- Div da parte de carnes do form -->				
+				<div id="carnes" class="col-md-4" align="center">
+					<img src="imagens/steak.png" height="80px" width="80px" align="center">
+					<h3>Carnes</h3>
+					<div class="form-group" align="left">
+						<br><br>
+						<!-- Checkbox de carnes -->	
+						<div class="checkbox">
+							<label><input type="checkbox" name="boi" value="1">Carne de Boi</label>
+						</div>
+						<div class="checkbox">
+							<label><input type="checkbox" name="porco" value="1">Carne de Porco</label>
+						</div>
+						<div class="checkbox">
+							<label><input type="checkbox" name="ling" value="1">Linguiça</label>
+						</div>
+						<div class="checkbox">
+							<label><input type="checkbox" name="frango" value="1">Frango</label>
+						</div>
+					</div><!-- Fim formgroup -->					
+				</div><!-- Fim da div carnes -->
+				<!-- Div da parte de descartáveis do form -->
+				<div id="descartaveis" class="col-md-4" align="center">
+					<img src="imagens/cutlery.png" height="80px" width="80px" align="center">
+					<h3>Descartáveis</h3>
+					<!-- checkbox de descartaveis -->
+					<div class="form-group" align="left">
 						<br><br>	
 						<div class="checkbox">
-						  <label><input type="checkbox" name="agua" value="on">Agua</label>
+							<label><input type="checkbox" name="prato" value="1">Pratos</label>
 						</div>
 						<div class="checkbox">
-						  <label><input type="checkbox" name="cerveja" value="1">Cerveja</label>
+							<label><input type="checkbox" name="copo" value="1">Copos</label>
 						</div>
 						<div class="checkbox">
-						  <label><input type="checkbox" name="refri" value="1">Refrigerante</label>
+							<label><input type="checkbox" name="talher" value="1">Talheres</label>
 						</div>
-						<div class="checkbox">
-						  <label><input type="checkbox" name="suco" value="1">Suco</label>
-						</div>
-				</div>
-			</div>
-			<div id="carnes" class="col-md-4" align="center">
-			<img src="imagens/steak.png" height="80px" width="80px" align="center">
-			<h3>Carnes</h3>
-				<div class="form-group" align="left">
-						<br><br>	
-						<div class="checkbox">
-						  <label><input type="checkbox" name="boi" value="1">Carne de Boi</label>
-						</div>
-						<div class="checkbox">
-						  <label><input type="checkbox" name="porco" value="1">Carne de Porco</label>
-						</div>
-						<div class="checkbox">
-						  <label><input type="checkbox" name="ling" value="1">Linguiça</label>
-						</div>
-						<div class="checkbox">
-						  <label><input type="checkbox" name="frango" value="1">Frango</label>
-						</div>
-				</div>	
-					
-					
-			
-			</div>
-			<div id="descartaveis" class="col-md-4" align="center">
-			<img src="imagens/cutlery.png" height="80px" width="80px" align="center">
-			<h3>Descartáveis</h3>
-			<div class="form-group" align="left">
-						<br><br>	
-						<div class="checkbox">
-						  <label><input type="checkbox" name="prato" value="1">Pratos</label>
-						</div>
-						<div class="checkbox">
-						  <label><input type="checkbox" name="copo" value="1">Copos</label>
-						</div>
-						<div class="checkbox">
-						  <label><input type="checkbox" name="talher" value="1">Talheres</label>
-						</div>
-				</div>
-			</div>
-			<center>
-				<button type="submit" name="insert" class="btn btn-success">Calcular</button>
-			</center>
-			</div>
-			
-		    </div>
-		</form>    
-	</body>
-	</html>
+					</div><!-- Fim formgroup -->
+				</div><!-- Fim da div descartáveis -->
+				<!-- Botão que envia os dados para resultado.php ( alinhado ao centro) -->
+				<center>
+					<button type="submit" name="insert" class="btn btn-success">Calcular</button>
+				</center>
+			</div><!-- Fim conteiner -->		
+		</form><!-- Fim do form -->  
+	</div><!-- Fim da div row -->  
+</body>
+</html>
--- resultado.php
+++ resultado.php
@@ -1,46 +1,47 @@
 <?php 
+//if para inserir 
 if(isset($_POST['insert'])){
 	$quantidade = $_POST['quantidade'];
 
-
+	//test para receber metodo post
 	if(isset($_POST["agua"]) == 1){
-		$agua = ($quantidade*500);	
+		$agua = (($quantidade*200)/1000);	
 	}else{
 		$agua = 0;
 	}
 	if (isset($_POST["cerveja"]) == 1) {
-		$cerveja = ($quantidade*2000);
+		$cerveja = (($quantidade*2000)/1000);
 	}else {
 		$cerveja = 0;
 	}
 	if (isset($_POST["refri"]) == 1) {
-		$refri = ($quantidade*1000);
+		$refri = (($quantidade*600)/1000);
 	}else {
 		$refri = 0;
 	}
 	if (isset($_POST["suco"]) == 1) {
-		$suco = ($quantidade*1000);
+		$suco = (($quantidade*600)/1000);
 	}else{
 		$suco = 0;
 	}
 
 	if(isset($_POST["boi"]) == 1){
-		$boi = ($quantidade*300);	
+		$boi = (($quantidade*300)/1000);	
 	}else{
 		$boi = 0;
 	}
 	if (isset($_POST["porco"]) == 1) {
-		$porco = ($quantidade*300);
+		$porco = (($quantidade*300)/1000);
 	}else {
 		$porco = 0;
 	}
 	if (isset($_POST["ling"]) == 1) {
-		$ling = ($quantidade*300);
+		$ling = (($quantidade*150)/1000);
 	}else{
 		$ling = 0;
 	}
 	if (isset($_POST["frango"]) == 1) {
-		$frango = ($quantidade*300);
+		$frango = (($quantidade*150)/1000);
 	}else{
 		$frango = 0;
 	}
@@ -61,149 +62,159 @@ if(isset($_POST['insert'])){
 		$talher = 0;
 	}
 	?>
+	<!-- Alert -->
 	<div class="alert alert-success">
 		Dados registrados com sucesso!
 	</div>
-
+	<!-- Inicio HTML -->
 	<!DOCTYPE html>
 	<html>
 	<head>
-		<title>Economia</title>
+		<title>Hoje é Dia de Rock</title>
+		<!-- Importaçoes -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="icon" href="imagens/DIADEROCK.png" type="image/x-icon"/>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<div class="row">
 			<div class="container" align="center">
+				<!-- Div com imagem "dia de rock" -->
 				<div align="center">
-				<a href="#" ><img src="imagens/DIADEROCK.png" height="200px" width="200px" align="center"></a></div>
-				
+				<a href="index.php" ><img src="imagens/DIADEROCK.png" height="200px" width="200px" align="center"></a></div>
+				<!-- Div para dividir colunas -->
 				<div id="bebidasresult" class="col-md-4" >
 				<?php 
-				if(isset($_POST["agua"]) == 1){
+				if(isset($_POST["agua"]) == 1){ //test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Agua</h2>
 				<a href="#"><img src="imagens/agua.png" height="150px" width="150px"></a>
-				<h3> $agua Ml.</h3>
+				<h3> $agua Litro(s)</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				<?php 
-				if(isset($_POST["cerveja"]) == 1){
+				  
+				if(isset($_POST["cerveja"]) == 1){//test para imprimir somente o que é usado
+					//abrir HTML no meio do PHP 
 					echo <<<HTML
 				<h2>Quantidade de Cerveja</h2>
 				<a href="#"><img src="imagens/b1.png" height="150px" width="180px"></a>
-				<h3> $cerveja Ml.</h3>
+				<h3> $cerveja Litro(s)</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				 <?php 
-				if(isset($_POST["refri"]) == 1){
+				  
+				if(isset($_POST["refri"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Refrigerante</h2>
 				<a href="#"><img src="imagens/refri.png" height="150px" width="150px"></a>
-				<h3> $refri Ml.</h3>
+				<h3> $refri Litro(s)</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				 <?php 
-				if(isset($_POST["suco"]) == 1){
+				 
+				if(isset($_POST["suco"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Suco</h2>
 				<a href="#"><img src="imagens/suco.png" height="150px" width="150px"></a>
-				<h3> $suco Ml.</h3>
+				<h3> $suco Litro(s)</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				
-				
+				?>
 				</div>
-
+				<!-- Div para dividir colunas -->
 				<div id="carnesresult" class="col-md-4">
 						<?php 
-				if(isset($_POST["boi"]) == 1){
+				if(isset($_POST["boi"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Carne de Boi </h2>
 				<a href="#"><img src="imagens/c1.png" height="150px" width="150px"></a>
-				<h3> $boi g.</h3>
+				<h3> $boi Kg</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				<?php 
-				if(isset($_POST["porco"]) == 1){
+				  
+				if(isset($_POST["porco"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Carne de Porco</h2>
 				<a href="#"><img src="imagens/porco.png" height="150px" width="150px"></a>
-				<h3> $porco g</h3>
+				<h3> $porco Kg</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				<?php 
-				if(isset($_POST["ling"]) == 1){
+				  
+				if(isset($_POST["ling"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Linguiça</h2>
 				<a href="#"><img src="imagens/ling.jpg" height="150px" width="150px"></a>
-				<h3> $ling g</h3>
+				<h3> $ling Kg</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				<?php 
-				if(isset($_POST["frango"]) == 1){
+				  
+				if(isset($_POST["frango"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Carne de Frango</h2>
 				<a href="#"><img src="imagens/frango.jpg" height="150px" width="180px"></a>
-				<h3> $frango g</h3>
+				<h3> $frango Kg</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-			
-				</div>
+				?>
+			    </div>
 
-
+			    <!-- Div para dividir colunas -->
 				<div id="descartaveisresult" class="col-md-4">
 				<?php 
-				if(isset($_POST["prato"]) == 1){
+				if(isset($_POST["prato"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Pratos</h2>
 				<a href="#"><img src="imagens/pratos.png" height="150px" width="150px"></a>
 				<h3> $prato Uni.</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				 		<?php 
-				if(isset($_POST["copo"]) == 1){
+				 
+				if(isset($_POST["copo"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Copos</h2>
 				<a href="#"><img src="imagens/copos.png" height="150px" width="150px"></a>
 				<h3> $copo Uni.</h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				 		<?php 
-				if(isset($_POST["talher"]) == 1){
+				 
+				if(isset($_POST["talher"]) == 1){//test para imprimir somente o que é usado 
+					//abrir HTML no meio do PHP
 					echo <<<HTML
 				<h2>Quantidade de Talheres</h2>
 				<a href="#"><img src="imagens/talheres.jpg" height="150px" width="150px"></a>
-				<h3> $talher Conjutos </h3>
+				<h3> $talher Conjuto(s) </h3>
+<!-- Fechar HTML ( não mover ) -->
 HTML;
 				}
-				 ?>
-				
-				
-				</div>
-
-				
-			
-				
-			</div>
-		</div>
+				?>
+				</div>		
+			</div><!-- Fim do conteiner -->
+		</div><!-- Fim da class row -->
+		<!-- Botão centralizado -->
 		<center><a href="index.php"><button class="btn btn-primary">Voltar</button></a></center>
 	</body>
 	</html>
-	<?php
+<?php
 }
 ?>
